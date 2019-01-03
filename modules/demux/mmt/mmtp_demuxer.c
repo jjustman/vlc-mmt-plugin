2ff30111919935b1219d70dff41eb4c5bfa4ac5a
Jason Justman <jjustman@sdg-komo-mac148.local>
2019-01-03 02:17:24 -0800

    struct/vec for fragmentation

--- modules/demux/mmt/mmtp_demuxer.c
+++ modules/demux/mmt/mmtp_demuxer.c
@@ -295,28 +295,81 @@ typedef struct
  *
  */
 
-typedef struct mmtp_mpu_type_header {
-	uint16_t mpu_payload_length;
-	uint8_t mpu_fragment_type;
-	uint8_t mpu_timed_flag;
-	uint8_t mpu_fragmentation_counter;
+/**
+ * base mmtp_packet_header fields
+ *
+ * clang doesn't know how to inherit from structs, e.g. -fms-extensions, so use a define instead
+ * see https://stackoverflow.com/questions/1114349/struct-inheritance-in-c
+ *
+ * typedef struct mmtp_packet_header {
+ *
+ * todo, ptr back to chain to mmtp_packet_id
+ */
 
-};
+#define _MMTP_PACKET_HEADER_FIELDS 			\
+	uint8_t mmtp_packet_version; 			\
+	uint8_t packet_counter_flag; 			\
+	uint8_t fec_type; 						\
+	uint8_t mmtp_payload_type;				\
+	uint8_t mmtp_header_extension_flag;		\
+	uint8_t mmtp_rap_flag;					\
+	uint8_t mmtp_qos_flag;					\
+	uint8_t mmtp_flow_identifer_flag;		\
+	uint8_t mmtp_flow_extension_flag;		\
+	uint8_t mmtp_header_compression;		\
+	uint8_t	mmtp_indicator_ref_header_flag;	\
+	uint8_t mmtp_type_of_bitrate;			\
+	uint8_t mmtp_delay_sensitivity;			\
+	uint8_t mmtp_transmission_priority;		\
+	uint16_t mmtp_header_extension_type;	\
+	uint16_t mmtp_header_extension_length;	\
+	uint16_t mmtp_packet_id; 				\
+	uint32_t mmtp_timestamp;				\
+	uint32_t packet_sequence_number;		\
+	uint32_t packet_counter;				\
+	block_t *raw_packet;					\
+
+
+//define for mpu type common header fields for struct inheritance
+//todo: add in MMTHSample box
+
+#define _MMTP_MPU_TYPE_HEADER_FIELDS 		\
+	_MMTP_PACKET_HEADER_FIELDS;				\
+	uint16_t mpu_payload_length;			\
+	uint8_t mpu_fragment_type;				\
+	uint8_t mpu_timed_flag;					\
+	uint8_t mpu_fragmentation_indicator;	\
+	uint8_t mpu_aggregation_flag;			\
+	uint8_t mpu_fragmentation_counter;		\
+	block_t *mpu_data_unit_payload;			\
 
 typedef struct {
+	_MMTP_MPU_TYPE_HEADER_FIELDS;
+	uint32_t movie_fragment_sequence_number;
+	uint32_t sample_number;
+	uint32_t offset;
+	uint8_t priority;
+	uint8_t dep_counter;
 
 } mpu_data_unit_payload_fragments_timed_t;
 
 typedef struct {
+	_MMTP_MPU_TYPE_HEADER_FIELDS;
+
+	uint32_t non_timed_mfu_item_id;
 
 } mpu_data_unit_payload_fragments_nontimed_t;
 
+typedef struct VLC_VECTOR(struct mpu_data_unit_payload_fragments_timed_t *) 	mpu_data_unit_payload_fragments_timed_vector_t;
+typedef struct VLC_VECTOR(struct mpu_data_unit_payload_fragments_nontimed_t *)	mpu_data_unit_payload_fragments_nontimed_vector_t;
 
 typedef struct {
-	mpu_data_unit_payload_fragments_timed_t timed_fragments_vector;
-	mpu_data_unit_payload_fragments_nontimed_t nontimed_fragments_vector;
+	mpu_data_unit_payload_fragments_timed_vector_t 		timed_fragments_vector;
+	mpu_data_unit_payload_fragments_nontimed_vector_t 	nontimed_fragments_vector;
 
 } mpu_data_unit_payload_fragments_t;
+
+typedef struct VLC_VECTOR(struct mpu_data_unit_payload_fragments_t *) mpu_data_unit_payload_fragments_vector_t;
 
 typedef struct {
 	struct mmtp_sub_flow_t *mmtp_sub_flow;
@@ -324,37 +377,56 @@ typedef struct {
 
 	//MPU Fragment type collections for reconstruction/recovery of fragments
 
-	//MPU metadata, mpu_fragment_type==0x00
-	mpu_data_unit_payload_fragments_t mpu_metadata_fragments_vector;
+	//MPU metadata, 							mpu_fragment_type==0x00
+	mpu_data_unit_payload_fragments_vector_t 	mpu_metadata_fragments_vector;
 
-	//Movie fragment metadata, mpu_fragment_type==0x01
-	mpu_data_unit_payload_fragments_t mpu_movie_fragment_metadata_vector;
+	//Movie fragment metadata, 					mpu_fragment_type==0x01
+	mpu_data_unit_payload_fragments_vector_t	mpu_movie_fragment_metadata_vector;
 
-	//MPU (media fragment_unit), mpu_fragment_type==0x02
-	mpu_data_unit_payload_fragments_t media_fragment_unit_vector;
+	//MPU (media fragment_unit),				mpu_fragment_type==0x02
+	mpu_data_unit_payload_fragments_vector_t	media_fragment_unit_vector;
 
 
 } mpu_fragments_t;
 
+/**
+ * todo:  impl's
+ */
 
-typedef struct VLC_VECTOR(struct mpu_fragments_t *) mpu_fragments_vector_t;
+typedef struct {
+	_MMTP_PACKET_HEADER_FIELDS;
+
+} generic_object_fragments_t;
+
+typedef struct {
+	_MMTP_PACKET_HEADER_FIELDS;
+} signalling_message_fragments_t;
+
+typedef struct {
+	_MMTP_PACKET_HEADER_FIELDS;
+} repair_symbol_t;
+
+typedef struct VLC_VECTOR(struct mpu_fragments_t *) 				mpu_fragments_vector_t;
+typedef struct VLC_VECTOR(struct generic_object_fragments_t *) 		generic_object_fragments_vector_t;
+typedef struct VLC_VECTOR(struct signalling_message_fragments_t *) 	signalling_message_fragments_vector_t;
+typedef struct VLC_VECTOR(struct repair_symbol_t *) 				repair_symbol_vector_t;
 
 typedef struct  {
 	uint16_t packet_id;
 
 	//mmtp payload type collections for reconstruction/recovery of payload types
 
-	//mpu (media_processing_unit):	paylod_type==0x00
-	mpu_fragments_vector_t mpu_fragments_vector;
+	//mpu (media_processing_unit):			paylod_type==0x00
+	mpu_fragments_vector_t 					mpu_fragments_vector;
 
-	//generic object:				payload_type==0x01
-	generic_object_fragments_vector_t generic_object_fragments_vector;
+	//generic object:						payload_type==0x01
+	generic_object_fragments_vector_t 		eneric_object_fragments_vector;
 
-	//signalling message: 			payload_type=0x02
-	signalling_message_fragments_vector_t signalling_message_fragements_vector;
+	//signalling message: 					payload_type=0x02
+	signalling_message_fragments_vector_t 	signalling_message_fragements_vector;
 
-	//repair symbol:				payload_type==0x03
-	repair_symbol_vector_t repair_symbol_vector;
+	//repair symbol:						payload_type==0x03
+	repair_symbol_vector_t 					repair_symbol_vector;
 
 } mmtp_sub_flow_t;
 
@@ -650,6 +722,9 @@ static int Open( vlc_object_t * p_this )
     MP4_Box_t       *p_ftyp;
     const MP4_Box_t *p_mvhd = NULL;
     const MP4_Box_t *p_mvex = NULL;
+
+    signalling_message_fragments_t z;
+
 
     bool      b_enabled_es;
     p_sys = calloc( 1, sizeof( demux_sys_t ) );
